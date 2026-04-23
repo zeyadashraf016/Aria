ec3f7a9b0c96ed855fc2a9b124656fe9fd673f84
zeyadashraf016 <zeyadashraf016@gmail.com>
2026-04-23 14:43:11 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../default_shadcn_theme.css";
+import "../styles/fonts.css";
+import "../styles/theme.css";
+import "../styles/tailwind.css";
+import "../styles/index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
